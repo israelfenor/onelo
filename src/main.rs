1de645959766fbd3d4c4e2020a3966b5e615ec25
Israel <israel@wiris.com>
2020-09-07 19:10:19 +0200

    First iteration of a function to get the name of all files with .md extension

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{fs, ffi::OsStr};
+
 fn main() {
-    println!("Hello Onelo");
+    get_markdown_files("test/files".to_string());
+}
+
+fn get_markdown_files (path: String) -> Vec<String> {
+    let mut markdown_files: Vec<String> = vec![];
+    
+    match fs::read_dir(path) {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(files) => for file in files {
+            match file {
+                Err(_) => {}
+                Ok(_) => {
+                    if file.unwrap().path().extension().and_then(OsStr::to_str) == Some("md") {
+                        println!("> MD");
+                        //markdown_files.push();
+                    }
+                }
+            }
+        }
+    }
+
+    markdown_files
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_only_md_files () {
+        let markdown_files = get_markdown_files("test/files".to_string());
+        assert_eq!(markdown_files.len(), 2);
+    }
 }

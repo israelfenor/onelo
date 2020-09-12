21296746ab98d59e077d49f06fc445751c209845
Israel Fenor <israelfenor@gmail.com>
2020-09-12 18:43:21 +0200

    refactor: use a private function to check if a file is a markdown file

--- src/files/mod.rs
+++ src/files/mod.rs
@@ -1,24 +1,39 @@
-use std::error::Error;
+use std::{error::Error, io::BufReader};
 use std::fs;
 use std::path::{Path, PathBuf};
 
 type Result<T, E = Box<dyn Error>> = std::result::Result<T, E>;
 
+/// Get all markdown files of a path
 pub fn get_markdown_files<P: AsRef<Path>>(path: P) -> Result<Vec<PathBuf>> {
     let mut paths = vec![];
 
     for result in fs::read_dir(path)? {
         let path = result?.path();
 
-        if let Some(ext) = path.extension() {
-            if ext == "md" {
-                paths.push(path);
-            }
+        if is_valid_file(&path) {
+            paths.push(path);
         }
     }
 
     Ok(paths)
 }
+
+/// Check if a file is a valid source
+fn is_valid_file(path: &PathBuf) -> bool {
+    if let Some(ext) = path.extension() {
+        if ext != "md" {
+            return false;
+        }        
+    }
+
+    true
+}
+
+// pub fn process_file<P: AsRef<Path>>(path: P) -> Result<String> {
+//     let file = fs::File::open("test/files/01.md")?;
+//     let reader = BufReader::new(file);
+// }
 
 #[cfg(test)]
 mod tests {
@@ -29,4 +44,10 @@ mod tests {
         let paths = get_markdown_files("test/files");
         assert_eq!(paths.unwrap().len(), 2);
     }
+
+    // #[test]
+    // fn test_read_markdown_file() {
+    //     read_markdown_file("test/files/01.md");
+    //     assert!(true);
+    // }
 }

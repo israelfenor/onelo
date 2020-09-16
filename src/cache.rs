76064b7efe85c026c61d9dde2b91021d639efd7a
Arnau Siches <asiches@gmail.com>
2020-09-16 08:00:14 +0100

    feat: Test cache bootstrap
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/cache.rs
+++ src/cache.rs
@@ -8,7 +8,7 @@ use std::include_str;
 use std::path::Path;
 
 /// Opens a SQLite database at the given path.
-pub fn connect(path: &Path) -> Result<Connection> {
+pub fn connect<P: AsRef<Path>>(path: P) -> Result<Connection> {
     let conn = Connection::open(path)?;
     // conn.pragma_update(None, "foreign_keys", &"off")?;
     conn.pragma_update(None, "journal_mode", &"wal")?;
@@ -17,7 +17,7 @@ pub fn connect(path: &Path) -> Result<Connection> {
 }
 
 /// WAL persists across connections, this ensures it is switched off.
-pub fn disconnect(conn: &Connection) -> Result<()> {
+pub fn clean(conn: &Connection) -> Result<()> {
     conn.pragma_update(None, "wal_checkpoint", &"restart")?;
     conn.pragma_update(None, "journal_mode", &"delete")?;
 
@@ -31,4 +31,42 @@ pub fn bootstrap(conn: &Connection) -> Result<()> {
     conn.execute_batch(&bootstrap)?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rusqlite::NO_PARAMS;
+
+    #[test]
+    fn bootstrap_cache() -> Result<()> {
+        let mut conn = connect(":memory:")?;
+
+        bootstrap(&conn)?;
+
+        let tx = conn.transaction()?;
+        let mut stmt = tx.prepare(
+            r#"
+            SELECT
+                name
+            FROM
+                sqlite_schema
+            WHERE
+                type = 'table'
+            ORDER BY 1;
+            "#,
+        )?;
+        let mut actual: Vec<String> = Vec::new();
+        let rows = stmt.query_map(NO_PARAMS, |row| row.get(0))?;
+
+        for row in rows {
+            actual.push(row?);
+        }
+
+        let expected = vec!["context", "source", "source_entry"];
+
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
 }
